09e7eebcf77380fe337420a3a5a595695283d7d9
Aman <itsamanali5@gmail.com>
2023-07-24 12:10:55 +0530

    okkkK

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
+class waqt{
+     isOver;
+     time;
+     constructor(p2){
+       this.isOver = false;
+       this.time = p2;
+       this.isPlayed = true; 
+     }
 
+}
 
 let timesArr = [];
 
-timesArr.push(new Date())
-
+// timesArr.push(new Date())
+// console.log(new Date());
 function addTimerr(){
 //     let hh = document.getElementById("hh").value;
 //     let mm = document.getElementById("mm").value;
 //     let ss = document.getElementById("ss").value;
 
-    console.log(hh,mm,ss);
+    
     let da = new Date();
     da.setHours(hh.value);
     da.setMinutes(mm.value);
     da.setSeconds(ss.value);
-  
-    timesArr.push(da);
+   
+    let obj = new waqt(da);
+    timesArr.push(obj);
    
 }
 
+let sound = new Audio("./alarm.mp3");
+function stop(e) {
+    sound.pause();
+   
+}
 setInterval(render,1000);
 
 let timers = document.getElementById("timers");
@@ -28,27 +43,37 @@ function render(){
     }else{
         timers.innerHTML=""
         timesArr.forEach((e)=>{
-            e.setSeconds(e.getSeconds()-1);
-            if(e.getSeconds()==0){
-                e.setMinutes(e.getMinutes()-1);
-                e.setSeconds(60);
+            if(!e.isOver){
+                e.time.setSeconds(e.time.getSeconds()-1);
 
-            }
-            if(e.getMinutes()==0){
-                e.setHours(e.getHours()-1);
-                e.setMinutes(60);
+                if(e.time.getSeconds()==0){
+                    e.time.setMinutes(e.time.getMinutes()-1);
+                    e.time.setSeconds(60);
 
+                }
+                if(e.time.getMinutes()==0){
+                    e.time.setHours(e.time.getHours()-1);
+                    e.time.setMinutes(60);
+
+                }
             }
             let div = document.createElement('div');
             div.className="row";
-            if(e.getHours()==0 && e.getMinutes()==0 && e.getSeconds()==0){
+            if(e.time.getHours()==0 && e.time.getMinutes()==0 && e.time.getSeconds()==0){
+                e.isOver = true;
+                if(e.isPlayed){
+                    sound.play();
+                    e.isPlayed = false;
+                }
+               
+                
                 div.className = "over";
                 div.innerHTML=`
     
                 <div class="time">
                    Timer is Over
                 </div>
-                <button  class="delBtn">Stop</button>
+                <button onclick = "stop()" class="delBtn">Stop</button>
              
             `
             }else{
@@ -57,9 +82,9 @@ function render(){
             div.innerHTML=`
     
                 <div class="time">
-                    <span><input type="number" min="0" max="60"  value="${e.getHours()}"></span>
-                    :<span><input type="number"  min="0" max="60"  value="${e.getMinutes()}"></span>:
-                    <span><input type="number" min="0" max="60"   value="${e.getSeconds()}"></span>
+                    <span><input type="number" min="0" max="60"  value="${e.time.getHours()}"></span>
+                    :<span><input type="number"  min="0" max="60"  value="${e.time.getMinutes()}"></span>:
+                    <span><input type="number" min="0" max="60"   value="${e.time.getSeconds()}"></span>
                 </div>
                 <button  class="delBtn">Stop</button>
              
